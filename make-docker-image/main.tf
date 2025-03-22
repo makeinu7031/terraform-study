e0a24e16b6de4189baa51806e49521db3434fdbb
makeinu7031 <makeinu7031@gmail.com>
2025-03-22 23:43:57 +0900

    add:2章まで

--- make-docker-image/main.tf
+++ make-docker-image/main.tf
@@ -1,26 +1,32 @@
 terraform {
-    required_providers {
-      docker = {
-        source = "kreuzwerker/docker"
-        version = ">= 3.0.0"
-      }
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = ">= 3.0.0"
     }
+  }
 }
 
 provider "docker" {
-    host = "unix:///var/run/docker.sock"
+  host = "unix:///var/run/docker.sock"
 }
 
 resource "docker_image" "nginx" {
-  name = "nginx:latest"
-  keep_locally = true
+  name         = "nginx:stable-alpine"
+  keep_locally = false
 }
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
-  name = "turorial"
+  name  = var.container_name
   ports {
     internal = 80
     external = 8000
   }
+}
+
+variable "container_name" {
+  default     = "tutorial"
+  type        = string
+  description = "The name of continer"
 }
--- make-docker-image/memo.md
+++ make-docker-image/memo.md
@@ -1,1 +1,36 @@
 # terraform
+
+
+## mac OSの場合のset up
+```
+brew tap hashicorp/tap
+brew install hashicorp/tap/terraform
+```
+
+### autocompleteの設定
+macOSではzshを使用しているため下記のエラー時は~/.zshrcファイルを作成することで解消できる
+```bash
+% terraform -install-autocomplete
+Error executing CLI: Did not find any shells to install
+% touch ~/.zshrc
+% terraform -install-autocomplete
+```
+
+
+## provider
+### docker provider
+https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs
+
+provider色々用意されている
+https://registry.terraform.io/browse/providers
+例えばcloudRunならこれ
+https://registry.terraform.io/modules/GoogleCloudPlatform/cloud-run/google/latest
+
+
+## terraform環境変数優先順位
+
+最終的な優先順位（高い順）
+- -var オプション（CLI）
+- 環境変数（TF_VAR_変数名）
+- terraform.tfvars や *.auto.tfvars
+- デフォルト値（variable の default）
